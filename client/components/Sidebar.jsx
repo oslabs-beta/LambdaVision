71531ce92a7632300cff4c852f8ca4200d5ea7d9
darrenestrada <50327423+darrenestrada@users.noreply.github.com>
2025-01-23 13:50:25 -0500

    SideBar Component

---
+++ client/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <ul>
+        <li>Active Errors</li>
+        <li>Resolved</li>
+        <li>Alert History</li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
