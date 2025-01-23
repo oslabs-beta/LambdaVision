af8b771cc1830ee296c408d91d904c443c2756c8
Jocelyn's Laptop <jocelynscodesmith@jocelyns-air.mynetworksettings.com>
2025-01-23 09:08:31 -0500

    create header

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Header from './Header'; //created by Jocelyn 1/19/24 
+import './App.css'; //created by Jocelyn 1/19/24 
 
 
 const App = () => {
     return (
         <div>
             <h1>LambdaLens</h1>
+            <Header/> 
         </div>
     )
 }
