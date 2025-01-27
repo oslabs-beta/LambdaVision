467a65ce1fe4fc49dadb065d13bb0dce5c57e88a
“Tiara <tiaravan234@gmail.com>
2025-01-27 15:23:25 -0500

    styling changes

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <aside className="bg-gray-800 text-white h-screen w-64 p-6">
-      <ul className="space-y-4">
-        <li className="text-lg font-semibold hover:text-blue-400 cursor-pointer">
-          Active Errors
+    <aside className='bg-gray-800 text-white h-screen w-64 p-6'>
+      <ul className='space-y-4'>
+        <li className='text-lg font-semibold hover:text-blue-400 cursor-pointer'>
+          <Link to='/'> Overview</Link>
         </li>
-        <li className="text-lg font-semibold hover:text-blue-400 cursor-pointer">
-        Resolved
+
+        <li className='text-lg font-semibold hover:text-blue-400 cursor-pointer'>
+          <Link to='/function-detail'> Functions</Link>
         </li>
-        <li className="text-lg font-semibold hover:text-blue-400 cursor-pointer">
-          Alert History
+
+        <li className='text-lg font-semibold hover:text-blue-400 cursor-pointer'>
+          <Link to='/errors'>Active Errors</Link>
         </li>
       </ul>
     </aside>
