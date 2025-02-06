195000786daed3be323d8646db49b646938d842f
Jocelyn's Laptop <jocelynscodesmith@jocelyns-air.mynetworksettings.com>
2025-02-06 15:14:33 -0500

    created more sections for the Metrics Overview Page with route

--- client/pages/MetricsOverview.jsx
+++ client/pages/MetricsOverview.jsx
@@ -3,19 +3,60 @@ import React from "react";
 import Chart from "../components/Chart";
 import MetricCard from "../components/MetricCard";
 import ErrorTable from "../components/ErrorTable";
+import {useState, useEffect} from 'react' 
+import axios from 'axios'
 
+    {/* MetricsOverview REACT Section */}
 const MetricsOverview = () => {
+  const[metrics, setMetrics] = useState([
+    {Invocations: 0, Errors:0, throttles:'0ms', ColdStartDuration:0},
+  ]);
+  
+  {/* MetricsOverview FUNCTION Section */}
+
+
+  const fetchFunctionMetrics = async (f) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/lambda/total-metrics`);
+        setMetrics(response.data);
+    } catch (error) {
+      console.error('There was an error getting metrics information', error);
+    }
+  };
+  useEffect(() => {
+  fetchFunctionMetrics();}, []);
+
+
   return (
     <div className="App flex">
       <div className="flex-1">
         <div className="p-6 space-y-6">
           {/* Metrics Summary Section */}
           <div className= "flex flex-wrap gap-4">    {/*"grid grid-cols-1 md:grid-cols-4 gap-6"*/}
-            <MetricCard title="Total Functions" metric="24" />
-            <MetricCard title="Total Invocations" metric="1.2M" />
-            <MetricCard title="Avg. Execution Time" metric="245ms" />
-            <MetricCard title="Error Rate" metric="0.02%" />
-          </div>
+
+          <MetricCard
+          title='Total Invocations'
+          metric={metrics.Invocations}
+        ></MetricCard>
+
+          <MetricCard
+          title='Error Rate'
+          metric={metrics.Errors}
+        ></MetricCard>
+
+            <MetricCard
+          title='Throttle Times'
+          metric={metrics.throttles}
+        ></MetricCard>
+
+            <MetricCard
+          title='Cold Start Times'
+          metric={metrics.ColdStartDuration}
+        ></MetricCard>
+
+        </div>
+
 
           {/* Charts Section */}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -32,6 +73,7 @@ const MetricsOverview = () => {
 };
 
 export default MetricsOverview;
+
 
 
 
