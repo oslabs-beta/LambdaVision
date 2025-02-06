62d86388c89b9c7e0411952bb6f5aba75c3ee856
“Tiara <tiaravan234@gmail.com>
2025-02-06 13:22:35 -0500

    settings page created

--- client/App.jsx
+++ client/App.jsx
@@ -5,25 +5,27 @@ import Header from "./components/Header";
 import ErrorAlerts from "./pages/ErrorAlerts";
 import MetricsOverview from "./pages/MetricsOverview";
 import FunctionDetail from "./pages/FunctionDetail";
+import SettingsPage from "./pages/Settings";
 
 const App = () => {
   return (
     <Router>
-      <div className="flex h-screen">
+      <div className='flex h-screen'>
         {/* Sidebar (Always Visible) */}
         <Sidebar />
 
         {/* Main Content */}
-        <div className="flex flex-col flex-grow">
+        <div className='flex flex-col flex-grow'>
           {/* Header (Always Visible) */}
           <Header />
 
           {/* Page Content */}
-          <main className="p-6 bg-gray-100 flex-grow">
+          <main className='p-6 bg-gray-100 flex-grow'>
             <Routes>
-              <Route path="/" element={<MetricsOverview />} />
-              <Route path="/errors" element={<ErrorAlerts />} />
-              <Route path="/function-detail" element={<FunctionDetail />} />
+              <Route path='/' element={<MetricsOverview />} />
+              <Route path='/errors' element={<ErrorAlerts />} />
+              <Route path='/function-detail' element={<FunctionDetail />} />
+              <Route path='/settings' element={<SettingsPage />} />
             </Routes>
           </main>
         </div>
